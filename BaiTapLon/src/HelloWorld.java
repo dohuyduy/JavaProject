df53cd1c517fef41101b272cf48bfc8596ee1a45
dohuyduy <duy.dohuy@gmail.com>
2015-04-09 01:17:24 +0700

    Tao phien lam viec

---
+++ BaiTapLon/src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+	public static void main (String[] args) 
+	{
+		int a = 5;
+		System.out.println(+a);
+		float b = (float) 96.97;
+		System.out.println(+b);
+		char m = 't';
+		System.out.println(+(char)m);
+		boolean c = true;
+		System.out.println(c);
+		
+		
+		
+	}
+
+}
+
+
